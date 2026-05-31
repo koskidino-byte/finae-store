fafa13789bee74567dab1a5c4f34298a6d64ff97
Dino <dino@byfinae.com>
2026-05-31 14:31:35 +0000

    Deploy new bundle with hardcoded API URL

--- artifacts/store/src/main.tsx
+++ artifacts/store/src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+<<<<<<< Updated upstream
+=======
+import { setBaseUrl } from "@workspace/api-client-react";
+
+setBaseUrl("https://finae-api.onrender.com");
+>>>>>>> Stashed changes
 
 createRoot(document.getElementById("root")!).render(<App />);
